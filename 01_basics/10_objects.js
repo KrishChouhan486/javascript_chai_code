9db74629c3de7ed7740ce3a3bd111f7e549014ce
Krish Chouhan <102061947+KrishChouhan486@users.noreply.github.com>
2024-06-20 06:32:47 +0000

    object part 2

--- 01_basics/10_objects.js
+++ 01_basics/10_objects.js
@@ -1,28 +1,86 @@
-const mySym  = Symbol("key33");
+// const mySym  = Symbol("key33");
 
-const jsUser = {
-    name: "krish",
-    age: 20,
-    [mySym] : "key44",
-    email: "krish@gmail.com",
-    isLoggedIn : false
-}
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser[mySym])
+// const jsUser = {
+//     name: "krish",
+//     age: 20,
+//     [mySym] : "key44",
+//     email: "krish@gmail.com",
+//     isLoggedIn : false
+// }
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser[mySym])
 
-jsUser.email = "krish.com"
+// jsUser.email = "krish.com"
 // Object.freeze(jsUser);
-jsUser.email = "krsmnsish.com"
-console.log(jsUser.email)
+// jsUser.email = "krsmnsish.com"
+// console.log(jsUser.email)
 
 
-jsUser.greeting = function(){
-    console.log("krish is don")
+// jsUser.greeting = function(){
+//     console.log("krish is don")
+// }
+// console.log(jsUser.greeting())
+
+// jsUser.greetingTwo = function(){
+//     console.log(`krish is xzcxdon , ${this.age}`)
+// }
+// console.log(jsUser.greetingTwo())
+
+
+// -------------object part 2-----------------------
+
+// const tin = new Object();
+const tin = {}
+tin.id = "123abc",
+tin.name = "krish",
+tin.isLoggeddIN = false
+// console.log(tin);
+
+// const person = {
+//     email:"krishchouhan4@gmail.com",
+//     fullname:{
+//         userfullname : {
+//             firstName: "krish",
+//             lastName: " chouhan",
+//         }
+//     }
+// }
+// console.log(person.fullname.userfullname.firstName)
+
+// const obj1 = {1: "1", 2: "b"}
+// const obj2 = {name: "krish", age: 18}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({},obj1, obj2)
+// const obj3 = {...obj1, ...obj2 }
+// console.log(obj3);
+
+// const person = [
+//     {
+//         name: "krish",
+//         age: 24
+//     }
+// ]
+// console.log(person[0].age)
+
+// console.log(tin)
+// console.log(Object.keys(tin))
+// console.log(Object.values(tin))
+// console.log(Object.entries(tin))
+// console.log(tin.hasOwnProperty('isLoggeddIN'));
+
+
+// -----------part 3-------------------
+
+const course = {
+    coursename: "js ",
+    price : "999",
+    courseInstructor: " krish"
 }
-console.log(jsUser.greeting())
+// course.courseInstructor
 
-jsUser.greetingTwo = function(){
-    console.log(`krish is xzcxdon , ${this.age}`)
-}
-console.log(jsUser.greetingTwo())
+const {courseInstructor: teacher}= course;
+console.log(teacher)
+
+
