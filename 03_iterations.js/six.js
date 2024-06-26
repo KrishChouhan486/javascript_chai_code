9cd34ffe5ae2c27cb6f0667a453ff470de629b37
Krish Chouhan <102061947+KrishChouhan486@users.noreply.github.com>
2024-06-26 19:03:32 +0000

    for each

---
+++ 03_iterations.js/six.js
@@ -0,0 +1,35 @@
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach(item => {
+//     console.log(item); 
+// });
+
+
+// function print(val) {
+//     console.log(val);
+// }
+// coding.forEach(print)
+
+// coding.forEach((item, index, arr)=>{
+// console.log(item, index, arr)
+// })
+
+
+const object = [
+    {
+        languageName : "java",
+        languageFile : "js"
+    },
+    {
+        languageName : "java",
+        languageFile : "js"
+    },
+    {
+        languageName : "java",
+        languageFile : "js"
+    }
+]
+
+object.forEach( val =>{
+    console.log(val.languageName)
+})
