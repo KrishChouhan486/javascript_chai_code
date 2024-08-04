c3dd1eafc6ee390bbb3155e74df413888e842d76
Krish Chouhan <102061947+KrishChouhan486@users.noreply.github.com>
2024-08-04 06:53:21 +0000

    events

--- 03_iterations.js/five.js
+++ 03_iterations.js/five.js
@@ -1,4 +1,11 @@
-const coding = ["js", "ruby", "java", "python", "cpp"]
-coding.forEach(element => {
-    console.log();
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+coding.forEach((item) => {
+    console.log(item); 
 });
+
+
+function print(val) {
+    console.log(val);
+}
+coding.forEach(printMe)
